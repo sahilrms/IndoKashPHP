2fb9b31e1b1d02884e43ff36c73cb27c3891c720
sahilHussain <sahil.hussain@travel2kashmir.com>
2024-04-15 11:51:16 +0530

    add project backend-ui connected

--- admin/addOurProjects.php
+++ admin/addOurProjects.php
@@ -5,70 +5,48 @@ include "../dbConnection.php"; // Include your database connection file
 $items_query = $conn->query("SELECT * FROM completed_projects");
 $existing_items = $items_query->fetch_all(MYSQLI_ASSOC);
 
-$project_images = $conn->query("select * from project_media");
-$existing_project_images = $project_images->fetch_all(MYSQLI_ASSOC);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     // Start a transaction to ensure either all or none of the operations are executed
     $conn->begin_transaction();
 
     // Delete existing entries from the table
-    $delete_stmt = $conn->prepare("DELETE FROM management");
+    $delete_stmt = $conn->prepare("DELETE FROM `completed_projects`");
     $delete_stmt->execute();
-    $target_dir = "../img/our_projects/";
+    // Insert data into completed_projects table
+    foreach ($_POST['items'] as $item) {
+        print_r($item);
+        printf("%s", $item['owner']);
+        $owner_name = $conn->real_escape_string($item['owner']);
+        $place = $conn->real_escape_string($item['place']);
+        $description = $conn->real_escape_string($item['description']);
+        $feedback = $conn->real_escape_string($item['feedback']);
+        $client_contact = $conn->real_escape_string($item['client_contact']);
+        $created_at = date('Y-m-d H:i:s');
+        $updated_at = date('Y-m-d H:i:s');
 
-    $files = glob($target_dir . '*'); // Get all file names in the directory
-    foreach ($files as $file) {
-        if (is_file($file)) {
-            unlink($file); // Delete the file
-        }
-    }
+        // Insert project details into completed_projects table
+        $sql = "INSERT INTO `completed_projects` ( `owner_name`, `place`, `description`, `feedback`, `client_contact`, `created_at`, `updated_at`)
+                VALUES ( '$owner_name', '$place', '$description', '$feedback', '$client_contact', '$created_at', '$updated_at')";
 
-    // Loop through each item submitted in the form
-    foreach ($_FILES['items']['tmp_name'] as $index => $tmp_name) {
-        // Check if the individual image file was uploaded successfully
-        if (isset($tmp_name['image']) && is_uploaded_file($tmp_name['image'])) {
-            // Extract the values for title, description, and image
-            $title = $_POST['items'][$index]['title'];
+        if ($conn->query($sql) === TRUE) {
+            $project_id = $conn->insert_id; // Get the ID of the inserted project
 
-            $description = $_POST['items'][$index]['description'];
-            $designation = $_POST['items'][$index]['designation'];
-
-
-            $target_dir = "../img/our_management/";
-            $original_file_name = basename($_FILES['items']['name'][$index]['image']);
-            $imageFileType = strtolower(pathinfo($original_file_name, PATHINFO_EXTENSION));
-            $target_file = $target_dir . uniqid('', true) . '.' . $imageFileType; // Generate a unique file name
-
-            if (move_uploaded_file($tmp_name['image'], $target_file)) {
-                // Insert the item into the database
-                $insert_stmt = $conn->prepare("INSERT INTO management (image_link, title,designation, description) VALUES (?, ?, ?,?)");
-                $insert_stmt->bind_param("ssss", $target_file, $title, $designation, $description);
-                $insert_stmt->execute();
-            } else {
-                echo "Failed to upload image.";
-                $conn->rollback(); // Rollback the transaction
-                exit; // Exit the script
-            }
+            $conn->commit();
+            echo '<div id="banner" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Items saved successfully!</strong>
+            <span class="block sm:inline">Your items have been added to the database.</span>
+            <span onclick="removeBanner()" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1 1 0 0 1-1.497 1.323l-2.145-2.275-2.145 2.275a1 1 0 1 1-1.496-1.324l2.206-2.34-2.206-2.339a1 1 0 0 1 1.496-1.324l2.145 2.275 2.145-2.275a1 1 0 0 1 1.497 1.323l-2.206 2.339 2.206 2.34a1 1 0 0 1 0 1.324z"/></svg>
+            </span>
+          </div>
+         ';
         } else {
-            echo "Image file not uploaded.";
-            $conn->rollback(); // Rollback the transaction
-            exit; // Exit the script
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
     }
-
-    // Commit the transaction if all operations were successful
-    $conn->commit();
-    echo '<div id="banner" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-    <strong class="font-bold">Items saved successfully!</strong>
-    <span class="block sm:inline">Your items have been added to the database.</span>
-    <span onclick="removeBanner()" class="absolute top-0 bottom-0 right-0 px-4 py-3">
-      <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1 1 0 0 1-1.497 1.323l-2.145-2.275-2.145 2.275a1 1 0 1 1-1.496-1.324l2.206-2.34-2.206-2.339a1 1 0 0 1 1.496-1.324l2.145 2.275 2.145-2.275a1 1 0 0 1 1.497 1.323l-2.206 2.339 2.206 2.34a1 1 0 0 1 0 1.324z"/></svg>
-    </span>
-  </div>
- 
-  ';
 }
+
 
 ?>
 
@@ -129,31 +107,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
                                 value="<?php echo $item['client_contact'] ?>" type="text"
                                 class="px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
                         </div>
-                        <div class="w-full py-2 flex flex-col">
-                            <label for="media_url_<?php echo $index ?>" class="text-gray-600">Media files:</label>
-                            <input id="media_url_<?php echo $index ?>" name="items[<?php echo $index ?>][media_url]"
-                                type="file" multiple
-                                class="px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
-                        </div>
-
-                        <?php
-                        foreach ($existing_items as $index => $item) {
-                            // Filter existing_project_images based on project_id
-                            $filtered_images = array_filter($existing_project_images, function ($image) use ($item) {
-                                return $image['project_id'] == $item['id'];
-                            });
-
-                            echo '<div class="flex flex-wrap gap-2">';
-                            // Check if there are any filtered images
-                            if (!empty($filtered_images)) {
-                                // Display all images for the project
-                                foreach ($filtered_images as $image) {
-                                    echo '<img src="' . $image['media_url'] . '" width=150 />';
-                                }
-                            }
-                            echo '</div>';
-                        }
-                        ?>
 
                     </div>
 
@@ -188,7 +141,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
             <div class="flex flex-wrap">
                             <div class="w-2/5 py-2 flex flex-col">
                                 <label for="owner_${itemCount}" class="text-gray-600">Orchard Owner:</label>
-                                <input id="owner_${itemCount}"  type="text" name="items[${itemCount}][owner_name]" required
+                                <input id="owner_${itemCount}"  type="text" name="items[${itemCount}][owner]" required
                                     class="px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300">
                             </div>
                             <div class="w-2/5 py-2 mx-auto flex flex-col">
